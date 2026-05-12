b061d822d62903d335e113477c611524d2be56e2
EduardoTerada <eduterada@hotmail.com>
2026-05-12 20:54:43 -0300

    Add files via upload

---
+++ JAVAAPIREST/blogPessoal/blogPessoal/src/main/java/SENAI/Ipiranga/com/blogPessoal/Model/UsuarioLogin.java
@@ -0,0 +1,67 @@
+package SENAI.Ipiranga.com.blogPessoal.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name="usuarioLogin")
+public class UsuarioLogin {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+	private String usuario;
+	private String senha;
+	private String token;
+	private String foto;
+	private String tipo;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
